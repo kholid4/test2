0010534602e09b29a248b940baa18738c7c5a600
kholid4 <168082392+kholid4@users.noreply.github.com>
2024-06-06 02:43:06 +0700

    Delete js directory

--- js/header.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    var nav = document.querySelector('header nav');
-    if (nav.style.display === 'flex') {
-        nav.style.display = 'none';
-    } else {
-        nav.style.display = 'flex';
-    }
-});
--- js/scroll.js
+++
@@ -1,9 +0,0 @@
-// efek scroll pada navbar
-var navbar = document.querySelector("header");
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 0) {
-    navbar.classList.add("navbar-scrolled");
-  } else {
-    navbar.classList.remove("navbar-scrolled");
-  }
-});
